03b5e3abc7340ee5eb5649bf08cf439cdd49c488
sirh3e <marvin.huber@bluewin.ch>
2021-04-13 15:56:28 +0200

    [MODIFIED] src/main.rs added proper error handling

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use std::env;
 use std::fs::File;
-use std::io::{stdout, Read, Result, Write};
+use std::{io, io::Result};
 
 fn main() -> Result<()> {
-    let mut buffer = vec![];
+    let stdout = io::stdout();
+    let mut handle = stdout.lock();
 
     env::args().skip(1).for_each(|path| {
-        File::open(path).map(|mut file| file.read_to_end(&mut buffer));
+        match File::open(path).and_then(|mut file| io::copy(&mut file, &mut handle)) {
+            Err(err) => eprintln!("{}", err),
+            _ => (),
+        }
     });
 
-    stdout().write_all(&buffer)?;
     Ok(())
 }
